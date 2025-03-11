5d68268402f541d898193be624cc63217e33471c
Ashutosh Tiwari <142420245+onlyashu2003@users.noreply.github.com>
2025-03-11 07:16:51 +0000

    DataTypes

---
+++ basic/_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+console.log(3
+    +
+    3); // Code readability should be high
+
+console.log("Ashutosh")
+
+let name = "Ashutosh"
+let age = 30
+let isLoggedIn = true
+let State  = null
+
+
+//number => 2 to power 53
+//BigInt => 2 to power 53 to 2 to power 64
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique value
+
+
+
+// object =>
+
+console.log(typeof "Ashutosh");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
